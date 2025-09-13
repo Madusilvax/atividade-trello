48b9ef8a48efb0a45dffe1ebae0882ff99d49f22
Maria Eduarda <ma.eferreira20@gmail.com>
2025-09-12 22:17:41 -0300

    exercicios 01,02,03

---
+++ Exercico001/app.js
@@ -0,0 +1,8 @@
+let nome="", idade= 0
+nome = prompt("informe seu nome: ")
+idade = prompt("informe sua idade: ")
+alert("Olá, seja bem vindo(a) " + nome + "\nVocê tem " + idade + "anos")
+
+console.log("olá, seja bem vindo(a) " + nome + "\nVocê tem " + idade + "anos")
+
+documento.write("Olá, seja bem vindo(a) " + nome + "\nVocê tem " + idade + "anos")
