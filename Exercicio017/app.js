c71b3e967d91813d127b315be103940b44cb86b3
Maria Eduarda <ma.eferreira20@gmail.com>
2025-10-31 22:26:56 -0300

    comecando a melhorar o site

--- Exercicio017/app.js
+++ Exercicio017/app.js
@@ -2,14 +2,14 @@ let contatos = []
 
 function fnSalvar(){
     let umContato = {
-        nome: document.getElementById('nome').value, 
-        fotodecapa: document.getElementById('fotodecapa').value,
-        ano: ducument.getElementById('ano').value, 
-        avaliacao: document.getElementById('avaliacao').value, 
-        trailer: document.getElementById('trailer').value, 
-        genero: document.getElementById('genero').value, 
-        duracao: document.getElementById('duracao').value,
-        descricao: document.getElementById('descricao').value
+        nome: document.getElementById('nome'). value, 
+        fotodecapa: document.getElementById('fotodecapa'). value,
+        ano: document.getElementById('ano'). value, 
+        avaliacao: document.getElementById('avaliacao'). value, 
+        trailer: document.getElementById('trailer'). value, 
+        genero: document.getElementById('genero'). value, 
+        duracao: document.getElementById('duracao'). value,
+        descricao: document.getElementById('descricao'). value
     }
 
     contatos.push(umContato)
@@ -24,14 +24,14 @@ function fnListar() {
         dados += `
         <div class="col-md-4 mb-4">
             <div class="card" style="width: 100%;">
-                <img src="${umContato.foto}" class="card-img-top" alt="Foto de ${umContato.nome}" style="height: 200px; object-fit: cover;">
+                <img src="${umContato.fotodecapa}" class="card-img-top" alt="Foto de ${umContato.fotodecapa}" style="height: 200px; object-fit: cover;">
                 <div class="card-body">
                     <h5 class="card-title">${umContato.nome}</h5>
                     <p class="card-text">
-                        <strong>Telefone:</strong> ${umContato.telefone} <br>
-                        <strong>Email:</strong> ${umContato.email}
+                        <strong>Ano:</strong> ${umContato.ano} <br>
+                        <strong>Avaliação:</strong> ${umContato.avaliacao}
                     </p>
-                    <a href="mailto:${umContato.email}" class="btn btn-danger">Excluir</a>
+                    <a href="mailto:${umContato.trailer}" class="btn btn-danger">Assitir Trailer</a>
                 </div>
             </div>
         </div>`;
